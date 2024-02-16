010af9895f36f959e3381b3612867469e304d282
Pranjali784 <114579659+Pranjali784@users.noreply.github.com>
2024-02-16 22:08:36 +0530

    Update yo.js

--- yo.js
+++ yo.js
@@ -1,17 +1,11 @@
-const valentines = document.querySelector('.valentines');
-const card = document.querySelector('.card');
-const letter = document.querySelector('.letter');
-
-let isOpen = false;
-
-valentines.addEventListener('click', () => {
-  if (!isOpen) {
-    card.classList.add('open');
-    letter.classList.add('out');
-    isOpen = true;
-  } else {
-    card.classList.remove('open');
-    letter.classList.remove('out');
-    isOpen = false;
-  }
+$(document).ready(function () {
+  $('.container').mouseenter(function () {
+      $('.card').stop().animate({
+          top: '-90px'
+      }, 'slow');
+  }).mouseleave(function () {
+      $('.card').stop().animate({
+          top: 0
+      }, 'slow');
+  });
 });
